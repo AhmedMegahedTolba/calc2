c0fb197d1f9f35051ce3d69079235bddd6115922
AhmedMegahedTolba <ahmed.megahed.tolba@gmail.com>
2023-10-25 19:22:07 +0300

    cal commit

---
+++ Release/App/Calc.d
@@ -0,0 +1,35 @@
+App/Calc.d App/Calc.o: ../App/Calc.c ../stdtypes.h \
+ ../Mcal/DIO_Interface.h ../Hal/LCD_Interface.h ../Utils.h \
+ ../Hal/KEYBAD_Interface.h ../CFG/KEYBAD_Cfg.h ../App/CALC.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+../stdtypes.h:
+
+../Mcal/DIO_Interface.h:
+
+../Hal/LCD_Interface.h:
+
+../Utils.h:
+
+../Hal/KEYBAD_Interface.h:
+
+../CFG/KEYBAD_Cfg.h:
+
+../App/CALC.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ main.c
@@ -0,0 +1,30 @@
+#include "StdTypes.h"
+#include "MemMap.h"
+#include "Utils.h"
+#include "DIO_Interface.h"
+#include "Seg_interface.h"
+#include "LCD_Interface.h"
+#include "KEYBAD_Interface.h"
+#include "EX_Interrupt.h"
+#include "ADC_Interface.h"
+#include "Sensors_Interface.h"
+#include "ADC_Service.h"
+
+
+#define F_CPU 16000000
+#include <util/delay.h>
+
+int main(void)
+{
+	
+	CALC_Init();
+	
+	
+    while (1) 
+    {
+		CALC_Runnable();
+		
+   
+  }
+
+	}
